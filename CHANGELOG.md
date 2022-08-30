2153bd0539f17dd000293dc9c2816ec3f38e4cf4
clitic <clitic21@gmail.com>
2022-08-30 19:10:18 +0530

    Updated build.rs

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# mp4decrypt Changelog (DD/MM/YYYY)
+
+## 0.1.0 (20/03/2022)
+
+Features:
+
+- Initial release
--- build.rs
+++ build.rs
@@ -1,20 +1,35 @@
 fn main() {
-    let bento4_source_path = std::path::PathBuf::from(std::env::var("BENTO4_SOURCE_PATH")
-    .expect("Download Bento4 (https://github.com/axiomatic-systems/Bento4/tags) source and store that path in BENTO4_SOURCE_PATH envoirnment variable."));
-    let ap4_lib_path = std::env::var("AP4_LIB_PATH").expect(
-        "Use AP4_LIB_PATH envoirnment variable to store path of ap4 library from Bento4 SDK.",
-    );
-
-    println!("cargo:rustc-link-search=native={}", ap4_lib_path);
-    println!("cargo:rustc-link-lib=static=ap4");
+    println!("cargo:rerun-if-changed=src/mp4decrypt.cpp");
+    println!("cargo:rerun-if-changed=src/mp4decrypt.h");
 
     cc::Build::new()
         .cpp(true)
         .file("src/mp4decrypt.cpp")
-        .include(bento4_source_path.join("Source/C++/Core"))
-        .include(bento4_source_path.join("Source/C++/Codecs"))
-        .include(bento4_source_path.join("Source/C++/Crypto"))
-        .include(bento4_source_path.join("Source/C++/MetaData"))
+        .files(
+            glob::glob("Bento4/Source/C++/Core/*.cpp")
+                .unwrap()
+                .map(|x| x.unwrap()),
+        )
+        .files(
+            glob::glob("Bento4/Source/C++/Codecs/*.cpp")
+                .unwrap()
+                .map(|x| x.unwrap()),
+        )
+        .files(
+            glob::glob("Bento4/Source/C++/Crypto/*.cpp")
+                .unwrap()
+                .map(|x| x.unwrap()),
+        )
+        .files(
+            glob::glob("Bento4/Source/C++/MetaData/*.cpp")
+                .unwrap()
+                .map(|x| x.unwrap()),
+        )
+        // .files(glob::glob("Bento4/Source/C++/System/StdC/*.cpp").unwrap().map(|x| x.unwrap()))
+        .include("Bento4/Source/C++/Core")
+        .include("Bento4/Source/C++/Codecs")
+        .include("Bento4/Source/C++/Crypto")
+        .include("Bento4/Source/C++/MetaData")
         .compile("decrypt");
 
     // let bindings = bindgen::Builder::default()
